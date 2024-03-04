34775266c83092453d4040c9af83e831d9db4cf2
user name <s2561635@eidf106-login.vms.os.eidf.epcc.ed.ac.uk>
2024-03-04 21:14:16 +0000

    update

--- lmeval_newtasks_runner.sh
+++ lmeval_newtasks_runner.sh
@@ -3,6 +3,7 @@ model_name=$1
 task=$2
 batch_size=$3
 save_file_name=$4
+include_path=$5
 
 
 python lm_eval \
@@ -12,4 +13,5 @@ python lm_eval \
         --device cuda \
         --batch_size ${batch_size} \
         --output_path /app/lm-eval-log/${save_file_name} \
+        --include_path ${include_path} \
         --log_samples
