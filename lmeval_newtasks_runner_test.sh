d9e6c8688e477c4aa958fecc6340bf7685608a4c
user name <s2561635@eidf106-login.vms.os.eidf.epcc.ed.ac.uk>
2024-03-10 22:52:23 +0000

    update

--- lmeval_newtasks_runner_test.sh
+++ lmeval_newtasks_runner_test.sh
@@ -1,10 +1,19 @@
-python  lm_eval \
+# Command line variables
+model_name=$1
+task=$2
+batch_size=$3
+save_file_name=$4
+include_path=$5
+limit_size=$6
+
+
+python lm_eval \
         --model hf \
-        --model_args pretrained=meta-llama/Llama-2-13b-chat-hf,parallelize=True \
-        --tasks biolama_ctd \
-        --device mps \
-        --batch_size auto \
-        --output_path /Users/vincent/Documents/GP-NLP/lm-eval-log/llama-2-13b-chat-biolama-ctd \
-        --include_path /Users/vincent/Documents/GP-NLP/lm-evaluation-harness-vt/probing_tasks_vt \
-        --log_samples\
-        --limit 50
+        --model_args pretrained=${model_name},parallelize=True \
+        --tasks ${task} \
+        --device cuda \
+        --batch_size ${batch_size} \
+        --output_path /app/lm-eval-log/${save_file_name} \
+        --include_path ${include_path} \
+        --log_samples \
+	--limit $limit_size
